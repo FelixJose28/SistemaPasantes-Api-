3a2d4fbf18e9ec82a8637e37f3dff684ad72062b
Freddy L P.B <freddy_luis_94@hotmail.com>
2021-03-09 10:42:04 -0400

    Se cambio app output type a exe debido a error cuando se ejecutaba

---
+++ SistemaPasantes.Api/Models/Convocatoria.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaPasantes.Api.Models
+{
+    public class Convocatoria
+    {
+        public int ID { get; set; }
+
+        public string Titulo { get; set; }
+
+        public int Cupo { get; set; }
+
+        public DateTime FechaInicio { get; set; }
+
+        public DateTime FechaCierre { get; set; }
+
+        public bool Terminada => DateTime.Now >= FechaCierre;
+    }
+}
