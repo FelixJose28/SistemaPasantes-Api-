58ba711e0b6a39e573fe167cf865c87e0c9ccbce
Gabriel Gil Geronimo <sr.gabigil@gmail.com>
2021-03-10 21:37:45 -0400

    Add startupObet

--- SistemaPasantes.Infrastructure/Repositories/TareaRepository.cs
+++ SistemaPasantes.Infrastructure/Repositories/TareaRepository.cs
@@ -8,19 +8,49 @@ using System.Threading.Tasks;
 
 namespace SistemaPasantes.Infrastructure.Repositories
 {
-    public class TareaRepository : ITareaRepository
+    public class TareaRepository : IGenericRepository<Tarea>
     {
-        private readonly sistemapasantesContext _context;
+        private readonly IGenericRepository<Tarea> _context;
 
-        public TareaRepository(sistemapasantesContext context) {
-            _context = context; 
+        public TareaRepository(IGenericRepository<Tarea> context)
+        {
+            _context = context;
         }
 
-        public async Task<IEnumerable<Tarea>> GetTarea()
-        { 
-            var tareas = await _context.Tareas.ToListAsync();
+        public async Task Add(Tarea entity)
+        {
+         /*   await _context.Add(entity);
 
-            return tareas; 
+            var saveResult = await _context.SaveChangesAsync(); 
+
+           return  saveResult == 1 ; 
+         */
+        }
+
+        public void AddNoAsync(Tarea entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Tarea> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Tarea> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Tarea entity)
+        {
+            throw new NotImplementedException();
         }
     }
+
 }
