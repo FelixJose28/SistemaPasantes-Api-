62f7304fdd410144a129b481b0c6caae7bea12c5
Felix Jose <felixjoseynfantequezada@gmail.com>
2021-03-18 18:55:18 -0400

    entregartarea post

--- SistemaPasantes.Api/Controllers/TareaEntregaController.cs
+++ SistemaPasantes.Api/Controllers/TareaEntregaController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SistemaPasantes.Core.DTOs;
+using SistemaPasantes.Core.entities;
 using SistemaPasantes.Core.Interfaces;
 using SistemaPasantes.Infrastructure.Data;
 using System;
@@ -32,6 +33,16 @@ namespace SistemaPasantes.Api.Controllers
             }
             var tareadto = _mapper.Map<IEnumerable<TareaEntregaDTO>>(allTareas);
             return Ok(tareadto);
+        }
+        [HttpPost]
+        public async Task<IActionResult> EntregarTarea(TareaEntregaDTO tareaEntregaDTO)
+        {
+            if (!ModelState.IsValid) return BadRequest("Modelo no valido");
+
+            var tarea = _mapper.Map<TareaEntrega>(tareaEntregaDTO);
+            await _unitOfWork.tareaEntregaRepository.Add(tarea);
+            await _unitOfWork.CommitAsync();
+            return Ok(tareaEntregaDTO);
         }
     }
 }
