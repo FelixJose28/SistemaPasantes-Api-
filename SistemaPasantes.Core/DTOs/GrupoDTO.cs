13e878ce38c265bc093cb5f8b15931b063a56e91
Felix Jose <felixjoseynfantequezada@gmail.com>
2021-03-22 11:52:08 -0400

    grupo and pasante dto created

---
+++ SistemaPasantes.Core/DTOs/GrupoDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SistemaPasantes.Core.DTOs
+{
+    public class GrupoDTO
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
---
+++ SistemaPasantes.Core/DTOs/PasanteDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SistemaPasantes.Core.DTOs
+{
+    public class PasanteDTO
+    {
+        public int Id { get; set; }
+        public int IdUsuario { get; set; }
+        public int IdConvocatoria { get; set; }
+    }
+}
